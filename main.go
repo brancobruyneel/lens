181b57881a521b3a03a054624c245000e7cc7411
brancobruyneel <branco.bruyneel@gmail.com>
2024-12-01 20:41:28 +0100

    chore: logs

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"log/slog"
+	"os"
 
 	"github.com/brancobruyneel/lens/mqtt"
 	"github.com/brancobruyneel/lens/tui"
@@ -9,6 +11,17 @@ import (
 )
 
 func main() {
+	out, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	defer out.Close()
+
+	logger := slog.New(slog.NewJSONHandler(out, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+
 	serverURL := "mqtt://127.0.0.1:1883"
 	mqttClient, err := mqtt.New(serverURL, "lens")
 	if err != nil {
